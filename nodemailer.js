b9b0c63de3ba7be70082b7ac705d8f80a255ed8b
Noah Yarborough <ndyarborough1997@gmail.com>
2020-05-26 01:22:04 -0400

    transporter fix

--- nodemailer.js
+++ nodemailer.js
@@ -12,7 +12,7 @@ const mailFunctions = {
             </footer>
             `;
         let transporter = nodemailer.createTransport({
-            host: 'smpt.gmail.com',
+            host: 'smtp.gmail.com',
             port: 465,
             secure: true, // true for 465, false for other ports
             auth: {
