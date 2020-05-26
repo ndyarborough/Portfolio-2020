1c734126f0469bbcfd61bd480e9a395bf1c32161
Noah Yarborough <ndyarborough1997@gmail.com>
2020-05-26 00:02:35 -0400

    heroku

--- server.js
+++ server.js
@@ -4,6 +4,7 @@ const port = process.env.PORT || 3000;
 const mailFunctions = require('./nodemailer.js');
 const bodyParser = require('body-parser')
 const cors = require('cors');
+const path = require('path');
 
 // parse application/x-www-form-urlencoded
 app.use(bodyParser.urlencoded({ extended: false }))
@@ -29,6 +30,21 @@ app.post('/sendEmail', (req, res) => {
             console.log(err)
             res.send(err)
         })
+})
+
+//Static file declaration
+app.use(express.static(path.join(__dirname, 'build')));
+
+//production mode
+if (process.env.NODE_ENV === 'production') {
+    app.use(express.static(path.join(__dirname, 'build')));
+    app.get('/*', (req, res) => {
+        res.sendfile(path.join(__dirname, 'build', 'index.html'));
+    })
+}
+//build mode
+app.get('/*', (req, res) => {
+    res.sendFile(path.join(__dirname + '/public/index.html'));
 })
 
 app.listen(port, () => console.log(`Example app listening at http://localhost:${port}`));
