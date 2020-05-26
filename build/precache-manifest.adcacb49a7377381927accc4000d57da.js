188ab1a8d855e8a8fd4d7a16f0670b45c94bd777
Noah Yarborough <ndyarborough1997@gmail.com>
2020-05-26 00:17:04 -0400

    add build folder

---
+++ build/precache-manifest.adcacb49a7377381927accc4000d57da.js
@@ -0,0 +1,118 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "7a0fd0495d1830b6f695b67b13d33b21",
+    "url": "/index.html"
+  },
+  {
+    "revision": "b8a7f91c5272d600f4c7",
+    "url": "/static/css/main.e47be467.chunk.css"
+  },
+  {
+    "revision": "fcae7804eacec10f7717",
+    "url": "/static/js/2.ded6cac3.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/static/js/2.ded6cac3.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "b8a7f91c5272d600f4c7",
+    "url": "/static/js/main.17aabd44.chunk.js"
+  },
+  {
+    "revision": "341ef3865b5b838c00be",
+    "url": "/static/js/runtime-main.4acdeb32.js"
+  },
+  {
+    "revision": "65758bfb0ed8907a6e71789a6d119935",
+    "url": "/static/media/DiplopiaEdinburgh.65758bfb.png"
+  },
+  {
+    "revision": "9165b110f77b23a9650986109a288c19",
+    "url": "/static/media/EventsHereNow.9165b110.png"
+  },
+  {
+    "revision": "580ffd26ccaa16481ed87c1829c74943",
+    "url": "/static/media/JSX-logo.580ffd26.png"
+  },
+  {
+    "revision": "791271eab4bf64f27c2ec0d0902b95b6",
+    "url": "/static/media/Sports Car Trivia.791271ea.png"
+  },
+  {
+    "revision": "286624fbc502982a097cc7a8b44fc7ac",
+    "url": "/static/media/about_icon.286624fb.png"
+  },
+  {
+    "revision": "6a62d2b8f0d02cc7e262738f0fe759af",
+    "url": "/static/media/bootstrap-logo.6a62d2b8.png"
+  },
+  {
+    "revision": "ca70257e0634582d4e07ea0edff6c10f",
+    "url": "/static/media/coding-bg.ca70257e.jpg"
+  },
+  {
+    "revision": "8b67d1064dc6b182de524fd187b0331f",
+    "url": "/static/media/express-logo.8b67d106.png"
+  },
+  {
+    "revision": "724794164fb289dd2f7d69dde7ac3bc0",
+    "url": "/static/media/github-logo.72479416.png"
+  },
+  {
+    "revision": "c8f9cf3d9804e1b49ed22ca5f82120fa",
+    "url": "/static/media/handlebars-logo.c8f9cf3d.png"
+  },
+  {
+    "revision": "df2ec1c3250aa9f6690cb8f965c170b3",
+    "url": "/static/media/jQuery-logo.df2ec1c3.png"
+  },
+  {
+    "revision": "94ae2dcd4fa410811cab4e1fbb403340",
+    "url": "/static/media/javascript-logo.94ae2dcd.png"
+  },
+  {
+    "revision": "e94e7b60e39fae48423a3a4ed3801469",
+    "url": "/static/media/linkedin-logo.e94e7b60.webp"
+  },
+  {
+    "revision": "d2b1e19091a3530dfb8bec06b5a926dc",
+    "url": "/static/media/material-ui-logo.d2b1e190.png"
+  },
+  {
+    "revision": "09ed2051ed0b6e784bede6e70c64e9e7",
+    "url": "/static/media/mongo-db-logo.09ed2051.png"
+  },
+  {
+    "revision": "096d33be2aad46efe516fcf6fece32ad",
+    "url": "/static/media/mongoose-logo.096d33be.png"
+  },
+  {
+    "revision": "754e01e49836a295574661a188161775",
+    "url": "/static/media/mysql-logo.754e01e4.png"
+  },
+  {
+    "revision": "999cb515b6f42efcb8f395108507c4ae",
+    "url": "/static/media/node-js-logo.999cb515.png"
+  },
+  {
+    "revision": "580ffd26ccaa16481ed87c1829c74943",
+    "url": "/static/media/react-logo.580ffd26.png"
+  },
+  {
+    "revision": "78381a33b521a32187d0c700e0d2a71e",
+    "url": "/static/media/sequelize-logo.78381a33.png"
+  },
+  {
+    "revision": "b8e0fd1237ba9418f8570910a2ed4c3b",
+    "url": "/static/media/skills_icon.b8e0fd12.png"
+  },
+  {
+    "revision": "c500a3801d8356a86da86a06c3d13a4d",
+    "url": "/static/media/webpack-logo.c500a380.png"
+  },
+  {
+    "revision": "9099704434ce0b616d97cce14399e461",
+    "url": "/static/media/white-texture.90997044.png"
+  }
+]);
