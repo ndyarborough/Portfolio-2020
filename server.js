061fa79b904eee3b52bfc45d1a982401f5b30550
Noah Yarborough <ndyarborough1997@gmail.com>
2020-05-26 00:50:45 -0400

    added client folder

--- server.js
+++ server.js
@@ -33,18 +33,18 @@ app.post('/sendEmail', (req, res) => {
 })
 
 //Static file declaration
-app.use(express.static(path.join(__dirname, 'build')));
+app.use(express.static(path.join(__dirname, 'client/build')));
 
 //production mode
 if (process.env.NODE_ENV === 'production') {
-    app.use(express.static(path.join(__dirname, 'build')));
+    app.use(express.static(path.join(__dirname, 'client/build')));
     app.get('/*', (req, res) => {
-        res.sendfile(path.join(__dirname, 'build', 'index.html'));
+        res.sendfile(path.join(__dirname, 'client', 'build', 'index.html'));
     })
 }
 //build mode
 app.get('/*', (req, res) => {
-    res.sendFile(path.join(__dirname + '/public/index.html'));
+    res.sendFile(path.join(__dirname + '/client/public/index.html'));
 })
 
 app.listen(port, () => console.log(`Example app listening at http://localhost:${port}`));
